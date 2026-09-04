c179d7524fae65e184c46446cba800db55b0f3d1
João Venceslau <jv028venceslau@gmail.com>
2026-09-04 17:17:45 -0300

    Delete ATIVIDADE 1 (HTML Injection) directory

--- ATIVIDADE 1 (HTML Injection)/prisma/migrations/20260903205729_criar_usuario/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "usuario" TEXT NOT NULL,
-    "senha" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_usuario_key" ON "User"("usuario");
